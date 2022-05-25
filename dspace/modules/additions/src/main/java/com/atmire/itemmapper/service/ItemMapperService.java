f85285864fa0fdbedbe38aaa56b7827a39c80ea3
Jens Vannerum <jens.vannerum@atmire.com>
2022-05-25 15:43:15 +0200

    91412: Feedback

--- dspace/modules/additions/src/main/java/com/atmire/itemmapper/service/ItemMapperService.java
+++ dspace/modules/additions/src/main/java/com/atmire/itemmapper/service/ItemMapperService.java
@@ -1,15 +1,11 @@
 package com.atmire.itemmapper.service;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Iterator;
-import java.util.List;
 
 import com.atmire.itemmapper.model.CuniMapFile;
 import com.atmire.itemmapper.model.GenericCollection;
-import com.atmire.itemmapper.model.Mapfile;
-import com.atmire.itemmapper.model.SourceCollection;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Collection;
 import org.dspace.content.Item;
@@ -30,8 +26,8 @@ public interface ItemMapperService {
     public void verifyParams(Context context, String operationmode, String sourceHandle, String destinationHandle,
                              String linkToFile, String pathToFile, boolean dryRun) throws SQLException;
 
-    public void reverseMappedItem(Context context, Item item, String sourceHandle, String destinationHandle,
-                                  boolean dryRun)
+    public void unmapItem(Context context, Item item, String sourceHandle, String destinationHandle,
+                          boolean dryRun)
         throws SQLException, AuthorizeException, IOException;
 
     public void showItemsInCollection(Context context, Collection collection) throws SQLException;
@@ -45,4 +41,15 @@ public interface ItemMapperService {
 
     public void mapItemsFromJson(Context context, Iterator<Item> items, CuniMapFile mapfile)
         throws SQLException, AuthorizeException;
+
+    public CuniMapFile getMapFileFromLink(String link) throws IOException;
+
+    public CuniMapFile getMapFileFromPath(String path) throws IOException;
+
+    public void mapFromParams(Context context, String destinationHandle, String sourceHandle, boolean dryRun) throws SQLException;
+
+    public void reverseMapFromParams(Context context, String destinationHandle, String sourceHandle, boolean dryRun) throws SQLException;
+
+    public void mapFromMappingFile(Context context, String link, String path)
+        throws IOException, SQLException, AuthorizeException;
 }
