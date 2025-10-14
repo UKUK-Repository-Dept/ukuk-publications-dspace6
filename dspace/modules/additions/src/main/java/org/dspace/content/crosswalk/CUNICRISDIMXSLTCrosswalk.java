cc52ceff10ce35e0ef3999d0d2fed75e5d06133e
Jakub Řihák <jakub.rihak@ruk.cuni.cz>
2025-10-14 17:08:37 +0200

    Testing custom XSLTIngestionCrosswalk plugin

---
+++ dspace/modules/additions/src/main/java/org/dspace/content/crosswalk/CUNICRISDIMXSLTCrosswalk.java
@@ -0,0 +1,17 @@
+package org.dspace.content.crosswalk;
+import org.apache.log4j.Logger;
+/** 
+ * Custom ingestion crosswalk that applies an XSLT transformation
+ * to DIM metadata during METS ingest. 
+ * 
+ * This class extends the standard XSLTIngestionCrosswalk and can be * registered under the alias "DIM" to override the default DIMIngestionCrosswalk. 
+*/
+public class CUNICRISDIMXSLTCrosswalk extends XSLTIngestionCrosswalk {
+
+    private static final Logger log = Logger.getLogger(CustomDIMXSLTCrosswalk.class);
+    
+    public CUNICRISDIMXSLTCrosswalk() {
+        super();
+        log.info("CUNICRISDIMXSLTCrosswalk initialized.");
+    }
+}
